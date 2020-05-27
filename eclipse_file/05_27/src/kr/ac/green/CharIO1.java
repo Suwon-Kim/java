512eeeb66479d86ee38d1750fd0cbef6b35f2153
kksksky01 <kksksky01@naver.com>
2020-05-27 19:58:08 +0900

    수업

--- eclipse_file/05_27/src/kr/ac/green/CharIO1.java
+++ eclipse_file/05_27/src/kr/ac/green/CharIO1.java
@@ -1,5 +1,20 @@
 package kr.ac.green;
 
-public class CharIO1 {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class CharIO1 {
+	// char - writer (char 타입 쓰기)
+	public static void writeToFile() {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("data.txt");
+			fw.write("하이~");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			MyUtils.closeAll(fw);
+		}
+		
+	}
 }
